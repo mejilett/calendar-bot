fee8d84e54b4e8ff181654eb87e92e2f087f5499
allenzfliu <liu3237@purdue.edu>
2025-11-26 22:25:04 -0800

    summary changes

--- framework.py
+++ framework.py
@@ -1,58 +1,80 @@
-#from webexteamssdk import WebexTeamsAPI
 import requests
-#import jsonify
+import time
+#import message_handler
 
-key = "" # bot token from discord
+BOT_TOKEN = ""
+PROJECT_ROOM_ID = "Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vNDE2MTM2ODAtY2E4MC0xMWYwLTgyNjAtNTVhZTBhYzkxNzYw"
 
-#api = WebexTeamsAPI(access_token=key)
-#id = api.people.me().id
-
-#app = Flask(__name__)
-
-def send_message_to_room(room_id: str, text: str):
-#  api.messages.create(roomId=room_id, markdown=text)
-  pass
-
-def handle_help_command(room_id: str):
-  help_text = "Hello!"
-  send_message_to_room(room_id, help_text)
-
-def handle_unknown(room_id: str):
-  send_message_to_room(room_id, "Unknown command.")
-
-def handle_message(text: str, room_id: str):
-  normalized = text.strip().lower()
-  if normalized in ("/help", "help"):
-    handle_help_command(room_id)
-  else:
-    handle_unknown()
-
-#@app.route('/webhook', methods=['POST'])
-def webhook():
-  webhook_data = request.json
-  handle_webhook(webhook_data)
-  return jsonify({"success": True})
-
-def authorized_get(endpoint):
+# simple authorized_get
+def authorized_get(endpoint, params=None):
   r = requests.get("https://webexapis.com/v1" + endpoint,
-                   headers = {"Authorization": "Bearer " + key})
+                   headers = {"Authorization": "Bearer " + BOT_TOKEN},
+                   params = params)
   return r
 
+def authorized_post(endpoint, params=None, data=None):
+  r = requests.post("https://webexapis.com/v1" + endpoint,
+                    headers = {"Authorization": "Bearer " + BOT_TOKEN, "Content-Type": "application/json"},
+                    params = params,
+                    post_data = data)
+
+# this returns the bot id and information
+# not terribly useful but good for debug on startup
 def get_self():
   r = authorized_get("/people/me")
+  if (r.status_code == 401):
+    print("You probably forgot to add the key!")
+    print("Exiting.")
+    raise SystemExit
   return r.json()
 
+# grabs the rooms the bot is in
+# for now just grab the first and use it
+# should only be in 1 anyway
 def get_rooms():
   api_fetch = authorized_get("/rooms")
   return api_fetch.json()['items']
 
-self_id = get_self()
-#print(get_self())
+# fetches a specific room by ID
+# this is so i can hardcode the room
+def get_room_by_id(rooms, id):
+  for room in rooms:
+    if (room['id'] == id):
+      return room
+  return None
 
-rooms = get_rooms()
-room = rooms[0]
-print(f"Found {len(rooms)} rooms. Selecting room {room['title']}")
+# gets the message
+# because of webex api, i can only get messages that mention the bot
+def get_message(room_id, count=1):
+  api_fetch = authorized_get("/messages", params = {"roomId": room_id, "max": count, "mentionedPeople": "me"})
+  return api_fetch.json()
 
-# main loop
-while True:
+# sends a message
+# assuming i did it right
+def post_message(room_id, text):
+  api_response = authorized_posts("/messages", data = {"roomId": room_id, "text": text})
+  return api_response.json()
+
+# this function starts up the bot and inits the main loop
+def main():
+  print("Hello World!")
+  bot_data = get_self()
+  self_id = bot_data['id']
+  bot_name = bot_data['displayName']
+  print(f"Started bot {bot_name}.")
+
+  rooms = get_rooms()
+  room = get_room_by_id(rooms, PROJECT_ROOM_ID)
+  print(f"Found {len(rooms)} rooms. Arbitrarilty selecting room {room['title']}")
+  print(f"Room ID: {room['id']}")
+
+  # main loop
+  # basically grabs messages and passes them to message handlers
+  while True:
+    poll_interval = 2000
+    time.sleep(poll_interval/1000) # 2 ms poll interval
+    message = get_message(room['id'])
+    
   
+
+main()
