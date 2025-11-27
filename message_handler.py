58851ee1e70740bea4742632130411d73311d64f
allenzfliu <liu3237@purdue.edu>
2025-11-26 23:40:19 -0800

    Create message_handler.py

---
+++ message_handler.py
@@ -0,0 +1,15 @@
+# registry of previously handled messages
+# since the API has no way to tell us we've already done this one
+previously_handled_messages = []
+BOT_NAME = "final-porject"
+
+def handle_message(message):
+  #print("Handling!")
+  #return "test message"
+  if (message['id'] in previously_handled_messages):
+    print("Message id " + message['id'] + " already seen. Skipping.")
+    return None
+  else:
+    # not in so we can process
+    # first add to list
+    previously_handled_messages.append(message['id'])
