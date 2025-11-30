6f1f87d778fa81e3e8e6af503e916f54e99c852f
Scarlet Mejia <scarlett.m443@gmail.com>
2025-11-29 16:56:22 -0800

    added calendar_id for delete event function

--- calendar_ops.py
+++ calendar_ops.py
@@ -42,5 +42,6 @@ def create_event(title, date_str, start_str, duration_min=60):
 #delete a scheduled meeting
 def delete_event(event_id):
     service = get_calendar_service()
-    service.events().delete(calendarID='primary', eventId=event_id).execute()
+    calendar_id = '' #put in primary google calendar ID
+    service.events().delete(calendarID=calendar_id, eventId=event_id).execute()
     return {'success': True, 'message': 'Event deleted successfully'}
